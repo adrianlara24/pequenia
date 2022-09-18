320f8bbb8b7f618ad45ceae65a72e097844599fe
adrianlara24 <adrianlara24@gmail.com>
2022-09-18 20:50:15 -0300

    estilos

--- component/Topbar.tsx
+++ component/Topbar.tsx
@@ -3,10 +3,14 @@ import css from "./Topbar.module.scss";
 
 const Topbar = () => {
   return (
-    <div>
-      <div>
-        <Link href='/'>Inicio</Link>
-        <Link href='/tienda'>Tienda</Link>
+    <div className={css.topbar}>
+      <div className={css.header}>
+        <img className={css.logo} src="logo.jpg" alt="Logo" />
+        <h2 className={css.titulo}>Mi Pequeña Belen</h2>
+      </div>
+      <div className={css.links}>
+        <Link href="/">Inicio</Link>
+        <Link href="/tienda">Tienda</Link>
       </div>
     </div>
   );
